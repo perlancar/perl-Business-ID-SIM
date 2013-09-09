6f4829f93a11831a8628117bf8f5a7135184b937
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2013-09-09 17:13:46 +0700

    Incompatible change: replace interface (validate_sim + OO) with Rinci-based parse_sim

--- lib/Business/ID/SIM.pm
+++ lib/Business/ID/SIM.pm
@@ -1,48 +1,16 @@
 package Business::ID::SIM;
-# ABSTRACT: Validate Indonesian driving license number (nomor SIM)
 
-=head1 SYNOPSIS
-
-    use Business::ID::SIM;
-
-    # OO-style
-
-    my $sim = Business::ID::SIM->new($str);
-    die "Invalid SIM!" unless $sim->validate;
-
-    print $sim->year_of_birth, "\n"; # also, yob()
-    print $sim->mon_of_birth, "\n"; # also, mob()
-    print $sim->area_code, "\n";
-    print $sim->serial, "\n";
-
-    # procedural style
-
-    validate_sim($str) or die "Invalid SIM!";
-
-=head1 DESCRIPTION
-
-This module can be used to validate Indonesian driving license number,
-Nomor Surat Izin Mengemudi (SIM).
-
-SIM is composed of 12 digits as follow:
-
- yymm.pp.aa.ssss
-
-yy and mm are year and month of birth, pp and aa are area code
-(province+district of some sort), ssss is 4-digit serial most probably
-starting from 1.
-
-Note that there are several kinds of SIM (A, B1, B2, C) but this is
-not encoded in the SIM number and all SIM's have the same number.
-
-=cut
-
+use 5.010001;
 use warnings;
 use strict;
+
 use DateTime;
+
 require Exporter;
 our @ISA = qw(Exporter);
-our @EXPORT = qw(validate_sim);
+our @EXPORT = qw(parse_sim);
+
+# VERSION
 
 # legend: S = lack of samples
 
@@ -73,189 +41,69 @@ my %provinces = (
 
     );
 
-=head1 METHODS
+our %SPEC;
 
-=head2 new $str
+$SPEC{parse_sim} = {
+    v => 1.1,
+    summary => 'Validate Indonesian driving license number (nomor SIM)',
+    args => {
+        sim => {
+            summary => 'Input to be parsed',
+            schema => 'str*',
+            pos => 0,
+            req => 1,
+        },
+    },
+};
+sub parse_sim {
+    my %args = @_;
 
-Create a new C<Business::ID::SIM> object.
+    my $sim = $args{sim} or return [400, "Please specify sim"];
+    my $res = {};
 
-=cut
+    $sim =~ s/\D+//g;
+    return [400, "Not 12 digit"] unless length($sim) == 12;
 
-sub new {
-    my ($class, $str) = @_;
-    bless {
-	_str => $str,
-	_err => undef, # errstr
-	_res => undef, # validation result cache
-	_dob => undef, # date is assumed to be 1
-    }, $class;
+    $res->{prov_code} = substr($sim, 4, 2);
+    return [400, "Unknown province code"] unless $provinces{$res->{prov_code}};
+    $res->{area_code} = substr($sim, 4, 4);
+
+    my ($y, $m) = $sim =~ /^(..)(..)/;
+    my $today = DateTime->today;
+    $y += int($today->year / 100) * 100;
+    $y -= 100 if $y > $today->year;
+    eval { $res->{dob} = DateTime->new(day=>1, month=>$m, year=>$y) };
+    return [400, "Invalid year and month of birth: $y, $m"] if $@;
+    $res->{serial} = $sim =~ /(....)$/;
+    return [400, "Serial starts from 1, not 0"] if $res->{serial} < 1;
+
+    [200, "OK", $res];
 }
-
-=head2 validate()
-
-Return true if SIM is valid, or false if otherwise. In the case of SIM
-being invalid, you can call the errstr() method to get a description
-of the error.
-
-=cut
-
-sub validate {
-    my ($self, $another) = @_;
-    return validate_sim($another) if $another;
-    return $self->{_res} if defined($self->{_res});
-
-    $self->{_res} = 0;
-    for ($self->{_str}) {
-	s/\D+//g;
-	if (length != 12) {
-	    $self->{_err} = "not 12 digit";
-	    return;
-	}
-	if (/^\d{4}(..)/ and !exists($provinces{$1})) {
-	    $self->{_err} = "unknown 'province' code";
-	    return;
-	}
-	my ($y, $m) = /^(..)(..)/;
-	my $today = DateTime->today;
-	$y += int($today->year / 100) * 100;
-	$y -= 100 if $y > $today->year;
-	eval { $self->{_dob} = DateTime->new(day=>1, month=>$m, year=>$y) };
-	if ($@) {
-	    $self->{_err} = "invalid year and month of birth: $y, $m";
-	    return;
-	}
-	/(....)$/;
-	if ($1 < 1) {
-	    $self->{_err} = "serial starts from 1, not 0";
-	    return;
-	}
-    }
-    $self->{_res} = 1;
-}
-
-=head2 errstr()
-
-Return validation error of SIM, or undef if no error is found. See
-C<validate()>.
-
-=cut
-
-sub errstr {
-    my ($self) = @_;
-    $self->validate and return;
-    $self->{_err};
-}
-
-=head2 normalize()
-
-Return formatted SIM, or undef if SIM is invalid.
-
-=cut
-
-sub normalize {
-    my ($self, $another) = @_;
-    return Business::ID::SIM->new($another)->normalize if $another;
-    $self->validate or return;
-    $self->{_str};
-}
-
-=head2 pretty()
-
-Alias for normalize().
-
-=cut
-
-sub pretty { normalize(@_) }
-
-=head2 area_code()
-
-Return 4-digit 'province'+district code component of SIM.
-
-=cut
-
-sub area_code {
-    my ($self) = @_;
-    $self->validate or return;
-    $self->{_str} =~ /^\d{4}(....)/;
-    $1;
-}
-
-=head2 year_of_birth()
-
-Return year component of the SIM, already added with the century, or
-undef if SIM is invalid.
-
-=cut
-
-sub year_of_birth {
-    my ($self) = @_;
-    $self->validate or return;
-    $self->{_dob}->year;
-}
-
-=head2 yob()
-
-Alias for year_of_birth()
-
-=cut
-
-sub yob { year_of_birth(@_) }
-
-=head2 month_of_birth()
-
-Return month component of the SIM, or undef if SIM is invalid.
-
-=cut
-
-sub month_of_birth {
-    my ($self) = @_;
-    $self->validate or return;
-    $self->{_dob}->mon;
-}
-
-=head2 mob()
-
-Alias for month_of_birth().
-
-=cut
-
-sub mob { month_of_birth(@_) }
-
-=head2 serial()
-
-Return 4-digit serial component of SIM, or undef if SIM is invalid.
-
-=cut
-
-sub serial {
-    my ($self) = @_;
-    $self->validate or return;
-    $self->{_str} =~ /(\d{4})$/;
-    $1;
-}
-
-=head1 FUNCTIONS
-
-=head2 validate_sim($string)
-
-Return true if SIM is valid, or false if otherwise. If you want to
-know the error details, you need to use the OO version (see the
-C<errstr> method).
-
-Exported by default.
-
-=cut
-
-sub validate_sim {
-    my ($str) = @_;
-    Business::ID::SIM->new($str)->validate();
-}
-
-=head1 BUGS/NOTES
-
-The list of valid 'province' codes in the program might need to be
-updated from time to time.
-
-=cut
 
 1;
+# ABSTRACT: Validate Indonesian driving license number (nomor SIM)
+
+=head1 SYNOPSIS
+
+ use Business::ID::SIM qw(parse_sim);
+
+ my $res = parse_sim(sim => "0113 40 00 0001");
+
+
+=head1 DESCRIPTION
+
+This module can be used to validate Indonesian driving license number, Nomor
+Surat Izin Mengemudi (SIM).
+
+SIM is composed of 12 digits as follow:
+
+ yymm.pp.aa.ssss
+
+yy and mm are year and month of birth, pp and aa are area code
+(province+district of some sort), ssss is 4-digit serial most probably starting
+from 1.
+
+Note that there are several kinds of SIM (A, B1, B2, C) but this is not encoded
+in the SIM number and all SIM's have the same number.
+
+=cut
--- t/01-basics.t
+++ t/01-basics.t
@@ -1,32 +1,52 @@
-#!perl -T
+#!perl
 
+use 5.010;
 use strict;
 use warnings;
-use Test::More tests => 15;
 
-use Business::ID::SIM;
+use Business::ID::SIM qw(parse_sim);
+use Data::Clean::JSON;
+use Test::More 0.98;
 
-ok(!(validate_sim("") ? 1:0), "procedural style (1)");
-ok((validate_sim("0101 06 00 0001") ? 1:0), "procedural style (2)");
+test_parse(
+    sim    => "0101 06 00 0001",
+    result => {
+        'area_code' => '0600',
+        'dob' => 978307200,
+        'prov_code' => '06',
+        'serial' => 1,
+    },
+);
+test_parse(
+    name   => 'invalid month',
+    sim    => "0113 40 00 0001",
+    status => 400,
+);
+test_parse(
+    name   => 'unknown province',
+    sim    => "0113 99 00 0001",
+    status => 400,
+);
 
-isa_ok(Business::ID::SIM->new(""), "Business::ID::SIM", "new() works on invalid SIM");
+DONE_TESTING:
+done_testing;
 
-ok(Business::ID::SIM->new("0101 06 00 0001")->validate, "valid SIM (1)");
+sub test_parse {
+    my %args = @_;
 
-ok(!Business::ID::SIM->new("0101 01 00 0001")->validate, "invalid SIM: unknown area (1)");
-ok(!Business::ID::SIM->new("0101 40 00 0001")->validate, "invalid SIM: unknown area (2)");
+    state $cleanser = Data::Clean::JSON->new;
 
-ok(!Business::ID::SIM->new("0113 40 00 0001")->validate, "invalid SIM: invalid month");
+    subtest +($args{name} //= "sim $args{sim}"), sub {
+        my $res = $cleanser->clean_in_place(parse_sim(sim => $args{sim}));
 
-ok(!Business::ID::SIM->new("0101 06 00 0000")->validate, "invalid SIM: zero serial");
+        my $st = $args{status} // 200;
+        is($res->[0], $st) or diag explain $res;
 
-is(Business::ID::SIM->new("0101 06 00 0001")->area_code, "0600", "area_code");
-
-is(Business::ID::SIM->new("0201 06 00 0001")->year_of_birth % 100, 2, "year_of_birth");
-is(Business::ID::SIM->new("0201 06 00 0001")->yob % 100, 2, "year_of_birth (alias, yob)");
-is(Business::ID::SIM->new("0203 06 00 0001")->month_of_birth, 3, "month_of_birth");
-is(Business::ID::SIM->new("0203 06 00 0001")->mob, 3, "month_of_birth (alias, mob)");
-
-is(Business::ID::SIM->new("0101 06 00 0001")->serial, "0001", "serial");
-
-is(Business::ID::SIM->new("0101 06 00 0001")->normalize, "010106000001", "normalize");
+        if (exists $args{result}) {
+            is_deeply($res->[2], $args{result}) or diag explain $res;
+        }
+        if ($args{posttest}) {
+            $args{posttest}->($res);
+        }
+    };
+}
